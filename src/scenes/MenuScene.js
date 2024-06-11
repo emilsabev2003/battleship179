85ec74bb058cddccba3a500b19abe4dd54258372
emilsabev2003 <150591482+emilsabev2003@users.noreply.github.com>
2024-06-11 12:28:50 -0700

    bg

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -5,12 +5,12 @@ class MenuScene extends Phaser.Scene {
 
     preload() {
         //load background image
-        this.load.image("menuBG", "./assets/battleship_menu_bg.png");
+        this.load.image("menuBG", "./assets/new_menu_bg.png");
     }
 
     create() {
         //display background image
-        this.backgroundImage = this.add.tileSprite(0, 0, 700, 700, "menuBG").setOrigin(0, 0);
+        this.backgroundImage = this.add.tileSprite(0, 0, 1290, 650, "menuBG").setOrigin(0, 0);
 
         //create play scene transition
         this.transitionToPlay = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
