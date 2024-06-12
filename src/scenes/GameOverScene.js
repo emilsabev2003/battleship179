8e78179a51ad68d5205a7688f54e84aa280fa789
emilsabev2003 <150591482+emilsabev2003@users.noreply.github.com>
2024-06-11 19:03:36 -0700

    game over scene

--- src/scenes/GameOverScene.js
+++ src/scenes/GameOverScene.js
@@ -4,14 +4,23 @@ class GameOverScene extends Phaser.Scene {
     }
 
     preload() {
-        
+        //load background image
+        this.load.image("GO_bg", "./assets/game_over_sprite.png");
     }
 
     create() {
+        //display background image
+        this.backgroundImage = this.add.tileSprite(0, 0, 1290, 650, "GO_bg").setOrigin(0, 0);
 
+        //create play scene transition
+        this.transitionToPlay = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
     }
 
     update() {
-        
+        //transition to play scene
+        if (this.transitionToPlay.isDown)
+        {
+            this.scene.start("PlayScene");
+        }
     }
 }
